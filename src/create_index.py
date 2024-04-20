c23528a4ba3ade9b43022d7fd9ae3d98335e356f
Pavan Chaitanya Penagamuri <pavan@pal-10-184-27-242.itap.purdue.edu>
2024-04-20 13:32:50 -0400

    fixed duplicate patches

--- src/create_index.py
+++ src/create_index.py
@@ -9,23 +9,28 @@ import numpy as np
 from src.constants import WEAVIATE_INDEX_NAME, PERSIST_DIR
 
 def create_client():
-    client = Client(
-        url="http://localhost:8080"
-    )
-    return client
+    return Client(url="http://localhost:8080")
 
 def store_test_data(test_data):
     test_file_name = '../data/test_file.npz'
     np.savez(test_file_name, test_data)
 
+def get_unique_patches(data):
+    seen = set()
+    unique_tuples = []
+    for tup in data:
+        if tup['patch'] not in seen:
+            unique_tuples.append(tup)
+            seen.add(tup['patch'])
+    return unique_tuples
 
-def load_docs(filename: str):
-    docs = []
-    
+def load_docs(filename: str):    
     data = []
     with open(filename, 'r') as f:
         for line in f:
             data.append(json.loads(line))
+
+    data = get_unique_patches(data)
     
     projects = []
     for data1 in data:
