158c33157ce745b5b9d21bded1f6481509607d86
Pavan Chaitanya Penagamuri <pavan@pal-10-184-24-172.itap.purdue.edu>
2024-04-30 18:49:10 -0400

    changed code reviewer

--- src/code_reviewer.py
+++ src/code_reviewer.py
@@ -4,7 +4,7 @@ from src.util import extract_json_from_text, yes_no, filter_docs
 from src.prompts import *
 
 class CodeReviewer:
-    def __init__(self, top_k=1, model_name='mistral', use_ollama=False, use_rag=True, use_yes_no=True, github_bot=False, use_train_store=False):
+    def __init__(self, top_k=5, model_name='mistral', use_ollama=False, use_rag=True, use_yes_no=True, github_bot=False, use_train_store=False):
         self.top_k = top_k
         self.use_ollama = use_ollama
         self.model_name = model_name
@@ -14,7 +14,7 @@ class CodeReviewer:
         # print('github bot', self.github_bot)
         self.retriever, self.llm = load_retriever_and_llm(top_k=top_k, model_name=model_name, use_ollama=use_ollama, use_train_store=use_train_store)
 
-    def is_review_needed(self, patch: str):
+    def is_review_needed(self, patch: str, return_prompt=False):
         prompt = ''
 
         if self.use_rag:
@@ -39,15 +39,21 @@ class CodeReviewer:
 
         try:
             if json_response['reviewNeeded'] == 'false':
-                return False
+                if return_prompt:
+                    return False, prompt
+                else:
+                    return False
             else:
-                return True
+                if return_prompt:
+                    return True, prompt
+                else:
+                    return True
         except Exception as ex:
             print('Error in Json: \n' + response)
             raise Exception('Json Parsing Failed')
 
 
-    def generate_review_comment(self, patch: str):
+    def generate_review_comment(self, patch: str, return_prompt=False):
         prompt = ''
 
         if self.use_rag:
@@ -88,6 +94,9 @@ class CodeReviewer:
             # print(json_response['reviewComment'])
             # print(json_response['lineNumber'])
             return json_response['reviewComment'], json_response['lineNumber']
+        
+        if return_prompt:
+            return json_response['reviewComment'], prompt
         
         return json_response['reviewComment']
     
